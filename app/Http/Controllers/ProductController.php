a34108e73e49bdb8571d9eb6425dfce1b84fe042
maxhandx7 <74608008+maxhandx7@users.noreply.github.com>
2023-06-08 11:29:24 -0500

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Product;
-use App\Category;
+use App\category;
 use App\Provider;
 use App\Http\Requests\Product\StoreRequest;
 use App\Http\Requests\Product\UpdateRequest;
